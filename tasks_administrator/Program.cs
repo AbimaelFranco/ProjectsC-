415cc0c8c2cf39450f232ea0c3a97b84f27010b3
Abimael Franco <3006701680101@ingenieria.usac.edu.gt>
2025-08-04 23:52:30 -0600

    createperesons function

--- tasks_administrator/Program.cs
+++ tasks_administrator/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Data.SqlClient;
+
+class Program
+{
+    static string connectionString = "Server=localhost;Database=tasks_administrator;Trusted_Connection=True;";
+
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("\n1. Crear persona \n2. Leer personas \n3. Actualizar persona \n4. Eliminar persona \n5. Salir");
+            Console.Write("Seleccione una opcion: ");
+            string option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "1":
+                    CreatePerson();
+                    break;
+                case "2":
+                    // ReadPerson();
+                    break;
+                case "3":
+                    // UpdatePerson();
+                    break;
+                case "4":
+                    // DeletePereson();
+                    break;
+                case "5":
+                    break;
+                default:
+                    Console.WriteLine("Opcion no valida");
+                    break;
+            }
+        }
+    }
+
+    static void CreatePerson()
+    {
+        Console.Write("Ingrese el nombre: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Ingrese el edad: ");
+        string age = Console.ReadLine();
+
+        using SqlConnection conn = new(connectionString);
+        conn.Open();
+
+        String query = "INSERT INTO Peresonas (Nombre, Edad) VALUES (@name, @age)";
+        using SqlCommand cmd = new(query, conn);
+        cmd.Parameters.AddWithValue("@name", name);
+        cmd.Parameters.AddWithValue("@age", age);
+
+        int filas = cmd.ExecuteNonQuery();
+        Console.WriteLine($"{filas} persona(s) creada(s)");
+
+
+
+    }
+}
