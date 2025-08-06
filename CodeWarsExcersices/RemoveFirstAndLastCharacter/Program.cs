2a9841e9e8fea8f9b2e7eb7e1c79422ea4f549cf
Abimael Franco <3006701680101@ingenieria.usac.edu.gt>
2025-08-05 21:35:41 -0600

    Contains methods for string manipulation.

---
+++ CodeWarsExcersices/RemoveFirstAndLastCharacter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+/// <summary>
+/// Contains methods for string manipulation.
+/// </summary>
+public class Kata
+{
+    /// <summary>
+    /// Removes the first and last characters from the given string.
+    /// </summary>
+    /// <param name="s">The original string with at least 2 characters.</param>
+    /// <returns>
+    /// A new string with the first and last characters removed.
+    /// Returns an empty string if the input string has exactly 2 or fewer characters.
+    /// </returns>
+    public static string Remove_char(string s)
+    {
+        string resultado = "";
+        if (s.Length <= 2)
+        {
+            return "";
+        }
+        else
+        {
+            for (int i = 1; i < s.Length - 1; i++)
+            {
+                resultado += s[i];
+            }
+            return resultado;
+        }
+    }
+}
