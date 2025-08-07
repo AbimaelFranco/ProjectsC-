1957daf2965cad6e8e522f653f91cf4ac035eb34
Abimael Franco <3006701680101@ingenieria.usac.edu.gt>
2025-08-07 14:47:53 -0600

    menu of activities status

---
+++ ActivitiesStatus/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.SqlClient;
+
+class Program
+{
+    static string connectionString = "Server=localhost; Database=activities_status; Trusted_Connection=True;";
+
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("\n1. Crear tarea \n2. Mostrar tareas \n3. Modificar tarea \n4. Eliminar tarea \n5. Salir");
+            Console.Wirte("Seleccione una opcion: ");
+
+            string option = Console.ReadLine();
+
+        }
+    }
+
+}
