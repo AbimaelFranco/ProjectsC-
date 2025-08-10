f9a86e754095a28c330dc7e41f803ad574c2254a
Abimael Franco <3006701680101@ingenieria.usac.edu.gt>
2025-08-09 19:07:00 -0600

    excercise of cartesian neighbors

---
+++ Cartesian_neighbors/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Returns a list of all eight neighboring points of the given (x, y) coordinate
+/// on a Cartesian grid with a step size of 1.
+/// Neighboring points include all adjacent points horizontally, vertically,
+/// and diagonally.
+/// </summary>
+/// <param name="x">The x-coordinate of the point.</param>
+/// <param name="y">The y-coordinate of the point.</param>
+/// <returns>
+/// A list of integer arrays, each containing two elements representing the
+/// x and y coordinates of a neighboring point.
+/// </returns>
+
+public class Kata
+{
+    public static IEnumerable<int[]> cartesianNeighbor(int x, int y)
+    {
+        int x_value = 0;
+        int y_value = 0;
+        List<int[]> lista = new List<int[]>();
+        for (int i = -1; i <= 1; i++)
+        {
+            for (int j = -1; j <= 1; j++)
+            {
+                if (i == 0 && j == 0) continue;
+                x_value = x + i;
+                y_value = y + j;
+                lista.Add(new int[] { x_value, y_value });
+            }
+        }
+        return lista;
+    }
+}
---
+++ Tests/Program.cs
@@ -0,0 +1,6 @@
+﻿int x = 0;
+int y = 0;
+List<int> coordenates = new List<int>();
+coordenates.Add(x);
+coordenates.Add(y);
+Console.WriteLine($"{coordenates}");
